e170818959a55eaf8fd843fab1953e030fcb31d6
asciito <ayax.cordova@aydev.mx>
2023-10-20 10:09:41 -0600

    test: change first two test

--- tests/Feature/InstallBrowserCommandTest.php
+++ tests/Feature/InstallBrowserCommandTest.php
@@ -1,29 +1,54 @@
 <?php
 
+use App\Facades\GoogleForTesting;
+use App\GoogleDownloadable;
 use Illuminate\Support\Facades\File;
-use Symfony\Component\Finder\Finder;
+use Illuminate\Support\Facades\Http;
 use function Pest\Laravel\artisan;
 
 it('download the latest browser version', function () {
+    Http::fake();
+
+    $google = GoogleForTesting::partialMock();
+    $downloadable = Mockery::mock(GoogleDownloadable::class);
+
+    $downloadable->shouldReceive('getVersion')
+        ->andReturn('200.0.0.0');
+
+    $downloadable->shouldReceive('getChromeBrowserURL')
+        ->andReturn('https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/200.0.0.0/linux64/chrome-linux64.zip');
+
+    $google->shouldReceive('getLatestVersion')
+        ->andReturn($downloadable);
+
     artisan('install:browser --latest')
-        ->expectsOutputToContain('Downloading Google Chrome Browser')
-        ->expectsOutputToContain('downloaded')
+        ->doesntExpectOutputToContain("There' no versions available for [200.0.0.0]")
+        ->expectsOutputToContain('Downloading Google Chrome Browser [200.0.0.0]')
+        ->expectsOutputToContain('Google Chrome Browser [200.0.0.0] downloaded')
         ->assertSuccessful();
-
-    $finder = new Finder;
-
-    $finder
-        ->directories()
-        ->in(join_paths(env('HOME'), '.google-for-testing'))
-        ->name('chrome*');
-
-    expect($finder->hasResults())
-        ->toBeTrue();
 });
 
-it('it download the browser version []', function () {
+it('it download the browser version [113.0.5672.0]', function () {
+    Http::fake();
 
-})->todo();
+    $google = GoogleForTesting::partialMock();
+    $downloadable = Mockery::mock(GoogleDownloadable::class);
+
+    $downloadable->shouldReceive('getVersion')
+        ->andReturn('113.0.5672.0');
+
+    $downloadable->shouldReceive('getChromeBrowserURL')
+        ->andReturn('https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/113.0.5672.0/linux64/chrome-linux64.zip');
+
+    $google->shouldReceive('getVersion')
+        ->andReturn($downloadable);
+
+    artisan('install:browser --ver=113.0.5672.0')
+        ->doesntExpectOutputToContain("There' no versions available for [113.0.5672.0]")
+        ->expectsOutputToContain('Downloading Google Chrome Browser [113.0.5672.0]')
+        ->expectsOutputToContain('Google Chrome Browser [113.0.5672.0] downloaded')
+        ->assertSuccessful();
+});
 
 it('download the browser on other path', function () {
 
